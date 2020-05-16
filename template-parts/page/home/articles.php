cc7b782b206a374e5543bbe191609a4a44f83365
Sujan Prajapati Maharjan <prajapati_sujan1@hotmail.com>
2020-05-16 20:19:40 +1000

    Change Custom

---
+++ template-parts/page/home/articles.php
@@ -0,0 +1,82 @@
+<!-- Fullwidth Section -->
+<section class="fullwidth margin-top-95 margin-bottom-0">
+
+    <!-- Box Headline -->
+    <h3 class="headline-box">Articles & Tips</h3>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-4">
+
+                <!-- Blog Post -->
+                <div class="blog-post">
+
+                    <!-- Img -->
+                    <a href="blog-post.html" class="post-img">
+                        <img src="<?php echo bloginfo('template_directory'); ?>/images/blog-post-01.jpg" alt="">
+                    </a>
+
+                    <!-- Content -->
+                    <div class="post-content">
+                        <h3><a href="#">8 Tips to Help You Finding New Home</a></h3>
+                        <p>Nam nisl lacus, dignissim ac tristique ut, scelerisque eu massa. Vestibulum ligula nunc, rutrum in malesuada vitae. </p>
+
+                        <a href="blog-post.html" class="read-more">Read More <i class="fa fa-angle-right"></i></a>
+                    </div>
+
+                </div>
+                <!-- Blog Post / End -->
+
+            </div>
+
+            <div class="col-md-4">
+
+                <!-- Blog Post -->
+                <div class="blog-post">
+
+                    <!-- Img -->
+                    <a href="blog-post.html" class="post-img">
+                        <img src="<?php echo bloginfo('template_directory'); ?>/images/blog-post-02.jpg" alt="">
+                    </a>
+
+                    <!-- Content -->
+                    <div class="post-content">
+                        <h3><a href="#">Bedroom Colors You'll Never Regret</a></h3>
+                        <p>Nam nisl lacus, dignissim ac tristique ut, scelerisque eu massa. Vestibulum ligula nunc, rutrum in malesuada vitae. </p>
+
+                        <a href="blog-post.html" class="read-more">Read More <i class="fa fa-angle-right"></i></a>
+                    </div>
+
+                </div>
+                <!-- Blog Post / End -->
+
+            </div>
+
+            <div class="col-md-4">
+
+                <!-- Blog Post -->
+                <div class="blog-post">
+
+                    <!-- Img -->
+                    <a href="blog-post.html" class="post-img">
+                        <img src="<?php echo bloginfo('template_directory'); ?>/images/blog-post-03.jpg" alt="">
+                    </a>
+
+                    <!-- Content -->
+                    <div class="post-content">
+                        <h3><a href="#">What to Do a Year Before Buying Apartment</a></h3>
+                        <p>Nam nisl lacus, dignissim ac tristique ut, scelerisque eu massa. Vestibulum ligula nunc, rutrum in malesuada vitae. </p>
+
+                        <a href="blog-post.html" class="read-more">Read More <i class="fa fa-angle-right"></i></a>
+                    </div>
+
+                </div>
+                <!-- Blog Post / End -->
+
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- Fullwidth Section / End -->
---
+++ template-parts/page/home/offer.php
@@ -0,0 +1,82 @@
+<!-- Fullwidth Section -->
+<section class="fullwidth margin-top-105" data-background-color="#f7f7f7">
+
+    <!-- Box Headline -->
+    <h3 class="headline-box">What are you looking for?</h3>
+
+    <!-- Content -->
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-3 col-sm-6">
+                <!-- Icon Box -->
+                <div class="icon-box-1">
+
+                    <div class="icon-container">
+                        <i class="im im-icon-Office"></i>
+                        <div class="icon-links">
+                            <a href="listings-grid-standard-with-sidebar.html">For Sale</a>
+                            <a href="listings-grid-standard-with-sidebar.html">For Rent</a>
+                        </div>
+                    </div>
+
+                    <h3>Apartments</h3>
+                    <p>Aliquam dictum elit vitae mauris facilisis, at dictum urna dignissim donec vel lectus vel felis.</p>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <!-- Icon Box -->
+                <div class="icon-box-1">
+
+                    <div class="icon-container">
+                        <i class="im im-icon-Home-2"></i>
+                        <div class="icon-links">
+                            <a href="listings-grid-standard-with-sidebar.html">For Sale</a>
+                            <a href="listings-grid-standard-with-sidebar.html">For Rent</a>
+                        </div>
+                    </div>
+
+                    <h3>Houses</h3>
+                    <p>Aliquam dictum elit vitae mauris facilisis, at dictum urna dignissim donec vel lectus vel felis.</p>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <!-- Icon Box -->
+                <div class="icon-box-1">
+
+                    <div class="icon-container">
+                        <i class="im im-icon-Car-3"></i>
+                        <div class="icon-links">
+                            <a href="listings-grid-standard-with-sidebar.html">For Sale</a>
+                            <a href="listings-grid-standard-with-sidebar.html">For Rent</a>
+                        </div>
+                    </div>
+
+                    <h3>Garages</h3>
+                    <p>Aliquam dictum elit vitae mauris facilisis, at dictum urna dignissim donec vel lectus vel felis.</p>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <!-- Icon Box -->
+                <div class="icon-box-1">
+
+                    <div class="icon-container">
+                        <i class="im im-icon-Clothing-Store"></i>
+                        <div class="icon-links">
+                            <a href="listings-grid-standard-with-sidebar.html">For Sale</a>
+                            <a href="listings-grid-standard-with-sidebar.html">For Rent</a>
+                        </div>
+                    </div>
+
+                    <h3>Commercial</h3>
+                    <p>Aliquam dictum elit vitae mauris facilisis, at dictum urna dignissim donec vel lectus vel felis.</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- Fullwidth Section / End -->
