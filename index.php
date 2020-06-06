54c8b3f64ce1fea81a532f0560548053e5158061
Sujan Prajapati Maharjan <prajapati_sujan1@hotmail.com>
2020-06-06 13:40:30 +1000

    gutenberg

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php get_header(); ?>
 
-
-
 <?php get_template_part("template-parts/page/home/banner"); ?>
 
 <?php get_template_part("template-parts/page/home/offer"); ?>
 
+<?php
+    $the_main_loop = new WP_Query();
+    // go main query
+    if($the_main_loop->have_posts()) {
+    while($the_main_loop->have_posts()) {
+    $the_main_loop->the_post();
 
+    // Display your loop content
+
+    } // endwhile
+    wp_reset_postdata(); // VERY VERY IMPORTANT
+    }
+?>
 <?php echo do_shortcode("[paig_property_list]"); ?>
 
 <?php get_template_part("template-parts/page/home/featured"); ?>
