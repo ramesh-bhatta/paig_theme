76c86bf6c9f77d0ba97ed5c56ba98fb33c57d241
Ramesh Bhatta <ramesh.paig@outlook.com>
2020-06-10 13:29:25 +1000

    search shortcode modified

--- template-parts/page/home/banner.php
+++ template-parts/page/home/banner.php
@@ -4,7 +4,7 @@
      data-color="#36383e" data-color-opacity="0.45" data-img-width="2500" data-img-height="1600">
     <div class="parallax-content">
 
-        <?php echo do_shortcode("[paig_property_search]"); ?>
+        <?php echo do_shortcode("[paig_property_search default_property_type='Build Contract']"); ?>
     </div>
 </div>
 
