2e11bfa321f48bbcb43398ae095d5173f90981df
Sujan Prajapati Maharjan <prajapati_sujan1@hotmail.com>
2020-07-04 12:26:36 +1000

    Fixed heaer

--- template-parts/page/home/banner.php
+++ template-parts/page/home/banner.php
@@ -1,5 +1,5 @@
 <div class="parallax" data-background="<?php echo get_the_post_thumbnail_url();?>"
-     data-color="#36383e" data-color-opacity="0.45" data-img-width="2500" data-img-height="1600">
+     data-color="#36383e" data-color-opacity="0.45" data-img-width="2500" data-img-height="1600" data-repeat="no-repeat">
     <div class="parallax-content">
         <?php echo do_shortcode("[paig_property_search]"); ?>
     </div>
