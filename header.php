e81bc135c2bb9e4cc3a67ef54d7ff6125b9c3821
Ramesh Bhatta <ramesh.paig@outlook.com>
2020-07-15 10:39:21 +1000

    Fix Logo URL

--- header.php
+++ header.php
@@ -8,12 +8,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="google" content="notranslate">
 
-	<!-- CSS ================================================== -->
-	<link rel="stylesheet" href="<?php echo bloginfo('template_directory'); ?>/css/style.css">
-	<link rel="stylesheet" href="<?php echo bloginfo('template_directory'); ?>/css/user.css">
-	<link rel="stylesheet" href="<?php echo bloginfo('template_directory'); ?>/css/color.css">
-
-
+    <?php wp_head(); ?>
 	<!-- Facebook Pixel Code -->
 	<script>
             ! function(f, b, e, v, n, t, s) {
@@ -41,10 +36,6 @@
             <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=201889034433353&ev=PageView&noscript=1" />
         </noscript>
         <!-- End Facebook Pixel Code -->
-
-
-
-	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -57,21 +48,21 @@
 
             <!-- Header -->
             <div id="header">
-                <div class="container flex flex-wrap items-center">
+                <div class="container flex flex-wrap items-center order">
 
                     <!-- Left Side Content -->
-                    <div class="left-side flex flex-wrap justify-between items-center">
+                    <div class="left-side flex flex-wrap justify-between items-center order-last md:order-none py-10 md:py-0">
 
                         <!-- Logo -->
-                        <div id="logo" class="margin-top-10">
+                        <div id="logo" class="py-3">
                             <!-- Logo -->
                             <?php the_custom_logo(); ?>
                             <?php
-                                $custom_logo_id = get_theme_mod( 'custom_logo' );
-                                $custom_logo_attr = array(
-                                    'class' => 'custom-logo',
-                                );
-                                $url=wp_get_attachment_image_url( $custom_logo_id, 'full', false )
+                            $custom_logo_id = get_theme_mod('custom_logo');
+                            $custom_logo_attr = array(
+                                'class' => 'custom-logo',
+                            );
+                            $url = wp_get_attachment_image_url($custom_logo_id, 'full', false)
                             ?>
                         </div>
 
@@ -89,15 +80,18 @@
 
                     <!-- Right Side Content / End -->
                     <div class="right-side">
-                        <div class="block md:flex flex-wrap items-center justify-end">
+                        <div class="block flex flex-wrap items-center justify-center md:justify-end">
                             <!-- Social Icons -->
-                            <ul class="social-icons mb-0">
+
+
+                            <ul class="social-icons mb-2 md:mb-0 flex justify-between">
                                 <?php $socialItems = getPaigSocialMedia();
                                 foreach ($socialItems as $socialItem) :
                                     if (!empty(getCustomThemeValue($socialItem))) :
                                         ?>
                                         <li>
-                                            <a class="<?php echo $socialItem ?>" target="_blank" href="<?php echo getCustomThemeValue($socialItem) ?>">
+                                            <a class="<?php echo $socialItem ?>" target="_blank"
+                                               href="<?php echo getCustomThemeValue($socialItem) ?>">
                                                 <i class="icon-<?php echo $socialItem ?>"></i>
                                             </a>
                                         </li>
@@ -105,8 +99,22 @@
                                 endforeach; ?>
 
                             </ul>
-                            <div class="login-hub">
-                                <a href="<?php echo getCustomThemeValue("login_url", "https://paighub.paig.com.au/auth/login") ?>" class="sign-in pl-5 pt-1"><i class="fa fa-user"></i> Log In / Register</a>
+
+                            <div class="login-hub w-1/2 md:w-auto">
+                                <a href="<?php echo getCustomThemeValue("login_url", "https://paighub.paig.com.au/auth/login") ?>"
+                                   class="sign-in pl-5 pt-1 px-16 block">
+                                    <i class="fa fa-user"></i> LOGIN</a>
+                            </div>
+                            <div class="hashtag-portal-logo flex items-center w-1/2  md:w-auto">
+                                <?php
+                                $link = getCustomThemeValue("hashtag_link","#");
+                                $hashtag_portal_img_url = getCustomThemeValue("hashtag_portal_logo","https://hashtagportal.com/");
+                                ?>
+                                <a href="<?php echo $link; ?>" target="_blank">
+
+                                    <img src="<?php echo $hashtag_portal_img_url;?>" class="w-64" />
+
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -116,16 +124,16 @@
 
                 <!-- Main Navigation -->
                 <!-- Main Navigation -->
-                <nav id="navigation" class="style-2">
+                <nav id="navigation" class="style-2 mt-0">
                     <div class="container">
                         <?php
-                            wp_nav_menu(array(
-                                'theme_location'   => 'primary_menu',
-                                'menu_class' => '',
-                                'container' => 'ul',
-                                'menu_id' => 'responsive',
-                                'walker' => new WP_Bootstrap_Navwalker()
-                            ));
+                        wp_nav_menu(array(
+                            'theme_location' => 'primary_menu',
+                            'menu_class' => '',
+                            'container' => 'ul',
+                            'menu_id' => 'responsive',
+                            'walker' => new WP_Bootstrap_Navwalker()
+                        ));
                         ?>
                     </div>
 
