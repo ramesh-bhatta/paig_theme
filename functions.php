7ba439368e5e9f962ed97583aca5e1396c6ef300
Sujan Prajapati Maharjan <prajapati_sujan1@hotmail.com>
2020-07-15 10:19:33 +1000

    Fix Theme Issue

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
-define("PAIG_THEME_VERSION","1.0");
+define("HASHTAG_THEME_VERSION","1.0");
+define("HASHTAG_STYLE_VERSION","1.0");
 define("PAIG_THEME_URL", get_template_directory_uri());
 
 require __DIR__ ."/inc/init.php";
