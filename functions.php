5caaa418573435ffe3460039d246a0d19cb2e7d8
Ramesh Bhatta <ramesh.paig@outlook.com>
2020-07-17 18:22:23 +1000

    fixed layout

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 define("HASHTAG_THEME_VERSION","1.5");
 define("HASHTAG_STYLE_VERSION","1.5");
 define("HASHTAG_SCRIPT_VERSION","1.5");
+
 define("PAIG_THEME_URL", get_template_directory_uri());
 
 require __DIR__ ."/inc/init.php";
