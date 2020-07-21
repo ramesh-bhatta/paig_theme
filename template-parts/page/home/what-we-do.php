e3d15a532863e4829d3ac755f9f6e43b7d260db8
Ramesh Bhatta <ramesh.paig@outlook.com>
2020-07-21 17:01:29 +1000

    theme color fixed

--- template-parts/page/home/what-we-do.php
+++ template-parts/page/home/what-we-do.php
@@ -1,10 +1,5 @@
-
-
-
-
 <!-- Fullwidth Section -->
-<section class="fullwidth margin-top-0 offer-section" data-background-color="#f7f7f7" id="what-we-do">
-
+<section class="fullwidth margin-top-0 offer-section py-24" data-background-color="#f7f7f7" id="what-we-do">
 
 
     <!-- Content -->
@@ -12,21 +7,29 @@
         <div class="row">
 
 
-
             <?php
             $post_id = get_option('page_on_front');
             $offer_arr = getOfferMetaValues($post_id);
 
+
+
             foreach ($offer_arr as $offer) :
-            ?>
+
+                ?>
 
                 <div class="col-md-3 col-sm-6">
                     <!-- Icon Box -->
                     <div class="icon-box-1">
-                        <div class="icon-container">
-                            <i class="<?php echo $offer['iconClass']; ?>"></i>
 
+                        <?php if ($offer['iconClass'] != null): ?>
+                            <div class="icon-container">
+                                <i class="<?php echo $offer['iconClass']; ?>"></i>
+                            </div>
+                        <?php elseif ($offer['icon'] !== null): ?>
+                        <div class="paig-imb-block">
+                            <img src="<?php echo $offer['icon']; ?>" class="object-fit"/>
                         </div>
+                        <?php endif; ?>
 
                         <h3><?php echo isset($offer['title']) ? $offer['title'] : ""; ?></h3>
                         <p><?php echo isset($offer['content']) ? $offer['content'] : ""; ?></p>
@@ -36,11 +39,10 @@
             <?php endforeach; ?>
 
 
-
         </div>
     </div>
 </section>
 <!-- Fullwidth Section / End -->
 
-<br />
+<br/>
 <!-- Fullwidth Section / End -->
